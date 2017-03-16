5418ffcfa8df3a753f9b205327213674f4f96065
afrancocaro <alejandro.franco.caro@gmail.com>
2017-03-16 18:39:05 +0100

    Create ejercicio-swap.py

---
+++ DEBERES JUEVES 16 MARZO/ejercicio-swap.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+a=mano_der="movil"
+b=mano_izq="bocadillo"
+print ("El valor de a: "+str(e)
+print ("El valor de b: "+str(b)
+mano_extra=mano_der
+mano_der=mano_izq
+mano_izq=mano_extra
+print ("El valor de a: "+str(a)
+print ("El valor de b: "+str(b)
