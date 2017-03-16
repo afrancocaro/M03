3037f7b2df8043569f2a0e35e6745ba382f8a82f
afrancocaro <alejandro.franco.caro@gmail.com>
2017-03-16 18:15:27 +0100

    Create ejercicio-mayor.py

---
+++ DEBERES JUEVES 16 MARZO/ejercicio-mayor.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+valor1=input("Indicar numero:")
+valor2=input("Indicar otro numero:")
+
+if (valor1 == valor2)
+	print ("Son iguales")
+else
+	if (valor1 > valor2)
+		print ("Valor 1 es >")
+	else
+		print ("Valor 2 es >")
