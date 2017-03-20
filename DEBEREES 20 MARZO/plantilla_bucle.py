e92108d258ab3de041d2d5b0b19ed18bc7d3728c
afrancocaro <alejandro.franco.caro@gmail.com>
2017-03-20 23:15:49 +0100

    Create plantilla_bucle.py

---
+++ DEBEREES 20 MARZO/plantilla_bucle.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+#Bucle
+
+while (salir == False):
+	print ("Hola")
+	
+tecla=raw_input 
+
+if (tecla == "S" or tecla == "s"):
+	print ("Adiós")
+if (salir == True):
