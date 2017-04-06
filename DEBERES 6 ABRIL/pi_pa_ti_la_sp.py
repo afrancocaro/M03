37f529475560026caa77c6771fb2367918483d8d
afrancocaro <alejandro.franco.caro@gmail.com>
2017-04-06 09:01:47 +0200

    Create pi_pa_ti_la_sp.py

---
+++ DEBERES 6 ABRIL/pi_pa_ti_la_sp.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+# Possibilitats: PE, PA, TI, LA, SP
+# Total 9: 5 empat, 20 guanyador
+# jugador1 humà
+# jugador2 machine
+
+from random import randint
+
+#Jugador humà
+jugador1=raw_input("Pon la jugada (PE/PA/TI/LA/SP):")
+
+
+#Jugador machine
+aleatori=randint(1,5)
+if (aleatori==1):
+	jugador2="PI"
+if (aleatori==2):
+	jugador2="PA"
+if (aleatori==3):
+	jugador2="TI"
+if (aleatori==3):
+    jugador2="TI"
+if (aleatori==4):
+    jugador2="LA"
+if (aleatori==5):
+    jugador2="SP"
+
+print ("Mi jugada es:", jugador 2)
+
+# Empat (5 combinacions)
+if (jugador1==jugador2):
+	print "Empate"
+ # 20 combinacions
+	# Guanya jugador1 (12 combinacions)     
+if 	(jugador1=="PI")and((jugador2=="LA")or(jugador2=="TI")or
+	(jugador1=="PA")and((jugador2=="PI")or(jugador2=="SP")or
+	(jugador1=="TI")and((jugador2=="PA")or(jugador2=="SP")or
+	(jugador1=="LA")and((jugador2=="PI"or(jugador2=="TI")or
+		):
+		print ("Gana jugador1")
+else: # Guanya jugador2 (3 combinacions)
+	print ("Has perdido !!")
