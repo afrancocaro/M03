ffd265eeff583d33f6268dabe4c967b6d7f63b9d
afrancocaro <alejandro.franco.caro@gmail.com>
2017-05-22 00:20:27 +0200

    Create contar_1-50.py

---
+++ contar_1-50.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+#El número por el que empieza a contar.
+numero = 1
+sortir = False
+while (sortir == False):
+#Imprime el numero que cuenta.
+	print "El número es", numero
+#Llega hasta el número 50.
+	if numero == 50:
+		sortir = True
+numero = numero + 1
