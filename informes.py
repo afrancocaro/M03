387c75f53fa7a9cd3b62470fa54c55a30e5f85d8
afrancocaro <alejandro.franco.caro@gmail.com>
2017-05-21 23:06:41 +0200

    Create informes.py

---
+++ informes.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+#Imprime los informes desde el año 2010 hasta el 2016.
+anyo = 2010
+sortir = False
+while (sortir==False):
+	#Imprime los informes desde el año 2010 hasta el 2016.
+		print "Informe del año", anyo
+  #Termina el bucle cuando llega a 2016.
+if (anyo == 2016):
+		sortir = True
+anyo=anyo +1
