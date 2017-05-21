785d604bb81c43c1330c4938c877752ef9e7a3fb
afrancocaro <alejandro.franco.caro@gmail.com>
2017-05-21 19:26:03 +0200

    Update ejercicio-mayor.py

--- DEBERES JUEVES 16 MARZO/ejercicio-mayor.py
+++ DEBERES JUEVES 16 MARZO/ejercicio-mayor.py
@@ -1,13 +1,13 @@
 # !/usr/bin/python
 # -*-coding: utf-8-*-
 
-valor1=input("Indicar numero:")
-valor2=input("Indicar otro numero:")
+valor1=input("Indicar numero (valor):")
+valor2=input("Indicar otro numero (valor):")
 
-if (valor1 == valor2)
-	print ("Son iguales")
-else
-	if (valor1 > valor2)
-		print ("Valor 1 es >")
-	else
-		print ("Valor 2 es >")
+if valor1 == valor2:
+	print ("Los numeros son iguales")
+else:
+	if valor1 > valor2:
+			print "El numero", valor1, "es mayor"
+	else:
+			print "El numero", valor2, "es mayor"
